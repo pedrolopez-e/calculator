bb0c312266783f55265188ee8f4d6f55fb40c395
Pedro López <pedrolopez.e@hotmail.com>
2022-03-20 15:27:57 -0300

    Creat operator function

---
+++ script.js
@@ -0,0 +1,24 @@
+const add = function(a,b) {
+    return a + b;
+}
+
+const substract = function(a,b) {
+    return a - b;
+}
+
+const multiply = function(a,b) {
+    return a * b;
+}
+
+const divide = function(a,b) {
+    return a / b;
+}
+
+const operator = function(a,b,f) {
+    return f(a,b);
+}
+
+
+
+
+
