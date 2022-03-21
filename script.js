9274680e047801851dffa480ec3a09fc31f46cc4
Pedro López <pedrolopez.e@hotmail.com>
2022-03-20 23:20:57 -0300

    Display buttons values on the screen

--- script.js
+++ script.js
@@ -18,6 +18,36 @@ const operator = function(a,b,f) {
     return f(a,b);
 }
 
+let value1;
+let value2;
+let operation;
+const numbers = document.querySelectorAll(".number");
+const screen = document.querySelector(".screen");
+const clear = document.querySelector(".clear")
+const operators = document.querySelector(".operator")
+
+numbers.forEach((number) => {
+    number.addEventListener('click', function(event) {
+        screen.textContent = event.target.textContent;
+    });
+});
+
+clear.addEventListener('click', function(event) {
+    screen.textContent = "";
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', function(event) {
+        bool = false;
+    });
+});
+
+
+
+
+
+
+
 
 
 
