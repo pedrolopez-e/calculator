8462d689dd603807543f8c2662280b5adddaf663
Pedro López <pedrolopez.e@hotmail.com>
2022-03-22 23:06:38 -0300

    Finish the calculator

--- script.js
+++ script.js
@@ -14,21 +14,30 @@ const divide = function(a,b) {
     return a / b;
 }
 
-const operator = function(a,b,f) {
+const operate = function(a,b,f) {
     return f(a,b);
 }
 
 let value1;
 let value2;
+let result;
 let operation;
 const numbers = document.querySelectorAll(".number");
 const screen = document.querySelector(".screen");
 const clear = document.querySelector(".clear")
-const operators = document.querySelector(".operator")
+const operators = document.querySelectorAll(".operator")
+const equal = document.querySelector(".equal");
 
 numbers.forEach((number) => {
     number.addEventListener('click', function(event) {
-        screen.textContent = event.target.textContent;
+        if (operation === undefined) {
+            screen.textContent += event.target.textContent;
+            value1 = Number(screen.textContent);
+        } else {
+            screen.textContent += event.target.textContent;
+            value2 = Number(screen.textContent);
+            result = operate(value1, value2, operation);
+        }        
     });
 });
 
@@ -36,15 +45,27 @@ clear.addEventListener('click', function(event) {
     screen.textContent = "";
 });
 
+equal.addEventListener('click', function(event) {
+    if (result != undefined) {
+        screen.textContent = result;
+        operation = undefined;
+    }
+})
+
 operators.forEach((operator) => {
     operator.addEventListener('click', function(event) {
-        bool = false;
+        screen.textContent = "";
+       if (event.target.textContent === "+") {
+           operation = add;
+       } else if (event.target.textContent === "-") {
+           operation = substract;
+       } else if (event.target.textContent === "x") {
+           operation = multiply;
+       } else if (event.target.textContent === "/") {
+           operation = divide;
+       };        
     });
 });
-
-
-
-
 
 
 
